f18edc05aa169c41fa57607e33bba17d8e3db3a4
Sravya <sravya26punati@gmail.com>
2025-01-23 19:32:25 +0530

    deleter create component

---
+++ src/components/CreateComponent.jsx
@@ -0,0 +1,147 @@
+import react, { useState } from 'react'
+import { createEmployee } from '../assets/services/EmployeeService';
+import { useNavigate } from 'react-router-dom';
+const CreateComponent = () => {
+
+  const [firstName, setfirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [emailId, setEmailId] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [error, setError] = useState({
+    firstName: '',
+    lastName: '',
+    emailId: '',
+    mobile: ''
+  });
+
+  const navigate = useNavigate();
+
+  const handleError = () => {
+    let valid = true;
+    const errorCopy = { ...error }; ///spread operator
+    if (firstName.trim()) {
+      errorCopy.firstName = ''
+    } else {
+      errorCopy.firstName = "First name cannot be empty"
+      valid = false;
+    }
+    if (lastName.trim()) {
+      errorCopy.lastName = ''
+    } else {
+      errorCopy.lastName = "Last name cannot be empty"
+      valid = false;
+    }
+    if (emailId.trim()) {
+      errorCopy.emailId = ''
+    } else {
+      errorCopy.emailId = "Email cannot be empty"
+      valid = false;
+    }
+    if (mobile.trim()) {
+      errorCopy.mobile = ''
+    } else {
+      errorCopy.mobile = "Mobile cannot be empty"
+      valid = false;
+    }
+    setError(errorCopy);
+    return valid;
+
+  }
+  // function onhandleFirstName(e){
+  //   setfirstName(e.target.value);
+  // }
+
+  // function onhandleLastName(e){
+  //   setLastName(e.target.value)
+  // }
+
+  // function onhandleEmail(e){
+  //   setEmail(e.target.value)
+  // }
+
+  // const onhandleMobile = (e)=>setMobile(e.target.mobile)
+
+
+  const addEmployee = (e) => {
+    console.log("form submitted")
+    e.preventDefault()  //prevents from reloading page
+    if (handleError()) {
+      const emp = { firstName, lastName, emailId, mobile }
+      // console.log("createcomponent",createEmployee(emp))
+      createEmployee(emp).then(async (response) => {
+        if (response.status === 400) {
+          console.log("ErrorStatus", response.status)
+          const res = await response.text();
+          alert(res)
+          console.log(emp)
+        } else {
+          const message = await response.text();
+          alert(message)
+          navigate("/employee")
+        }
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="card shadow" style={{ width: "25rem" }}>
+        <div className="card-body mb">
+          <h5 className="card-title text-center">Add User</h5>
+          <form>
+            <div className="form-group mb-3">
+              <label>First Name</label>
+              <input
+                type="text"
+                className={`form-control ${error.firstName ? 'is-invalid' : ''}`}
+                placeholder="Enter your name"
+                name="firstName"
+                value={firstName}
+                onChange={(e) => setfirstName(e.target.value)}
+              />
+              <div className='invalid-feedback'>{error.firstName}</div>
+            </div>
+            <div className="form-group mb-3">
+              <label>Last Name</label>
+              <input type="text"
+                className={`form-control ${error.lastName ? 'is-invalid' : ''}`}
+                placeholder="Enter your name"
+                name='lastName'
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              {error.lastName && <div className='invalid-feedback'>{error.lastName}</div>}
+            </div>
+            <div className="form-group mb-3">
+              <label>Email</label>
+              <input type="email"
+                className="form-control"
+                placeholder="Enter email"
+                name='emailId'
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </div>
+            <div className="form-group mb-3">
+              <label>Mobile Number</label>
+              <input type="text"
+                className="form-control"
+                placeholder="Enter your mobile number"
+                name='mobile'
+                value={mobile}
+                onChange={(e) => setMobile(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary w-100" onClick={addEmployee}>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateComponent;
